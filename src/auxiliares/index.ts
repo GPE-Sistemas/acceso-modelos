e940c39db07f09cf4913073bee08e9c23ec7c264
Fernando Cortese <cortesefernando@gmail.com>
2026-04-22 10:30:12 -0300

    feat: eliminar exportación de 'socket-message' en index.ts

--- src/auxiliares/index.ts
+++ src/auxiliares/index.ts
@@ -2,7 +2,6 @@ export * from './exactly';
 export * from './geojson';
 export * from './listado';
 export * from './query-param';
-export * from './socket-message';
 export * from './update-many';
 export * from './counters';
 export * from '../externos/eventos-lora';
